61f059823c119a880a368fdd698e3c3604886f8f
frknuzn <frknuzn34@hotmail.com>
2019-08-15 12:41:36 +0300

    Basit Stok Back end second commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into users (id,uname,pwd,name_surname,email) values (1000,'frknuzn','34furkan34','Furkan UZUN','frknuzn34@hotmail.com');
+insert into users (id,uname,pwd,name_surname,email) values (1000,'frknuzn','$2a$10$iQ2zN6GGUNYWN6.GeCZpyuCTaeTjogw2Sr5nA/cKRqdiL3hnQl9hS','Furkan UZUN','frknuzn34@hotmail.com');
 insert into users (id,uname,pwd,name_surname,email) values (1001,'dnzuzn','34deniz34','Deniz UZUN','deniz@uzun.com');
